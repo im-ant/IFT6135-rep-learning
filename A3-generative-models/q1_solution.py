356efe6596e7ce3ffb13281b299f0a48d21e0c91
im-ant <anthony.gx.chen@gmail.com>
2020-04-23 13:21:57 -0400

    q1 completed solutions, passed all autograder tests. a few inor notes (see TODO) to potentially fix

--- A3-generative-models/q1_solution.py
+++ A3-generative-models/q1_solution.py
@@ -51,10 +51,12 @@ def log_likelihood_normal(mu, logvar, z):
 
     # ==
     # Gaussian log likelihood
-    # TODO: recheck my derivation of the log likelihood is correct
-    ele_rel = logvar - ((z-mu).pow(2) / (2*logvar.exp()))  # (batch, input)
+    ele_rel = logvar + ((z-mu).pow(2) / logvar.exp())  # (batch, input)
     ele_inv_ll = np.log(2*np.pi) + ele_rel  # add the log2pi constant
     batch_ll = (-1.0/2.0) * torch.sum(ele_inv_ll, dim=1)  # (batch, )
+
+    # NOTE maybe TODO: recheck my derivation of the log likelihood is correct
+    # passed unit test but just to be sure
 
     # log normal
     return batch_ll
@@ -73,8 +75,17 @@ def log_mean_exp(y):
     batch_size = y.size(0)
     sample_size = y.size(1)
 
+    # ==
+    # log mean exp
+    ymax = y.max(dim=1, keepdim=True)[0]  # (batch, 1)
+    exp_y = (y-ymax).exp()  # (batch, sample)
+    sum_exp = torch.sum(exp_y, dim=1)  # (batch, )
+    lme = ((1.0/sample_size) * sum_exp).log() + ymax.view(-1)
+
+    # NOTE maybe TODO: check mean is outside of sum (passed test but be sure)
+
     # log_mean_exp
-    return
+    return lme
 
 
 def kl_gaussian_gaussian_analytic(mu_q, logvar_q, mu_p, logvar_p):
@@ -96,8 +107,15 @@ def kl_gaussian_gaussian_analytic(mu_q, logvar_q, mu_p, logvar_p):
     mu_p = mu_p.view(batch_size, -1)
     logvar_p = logvar_p.view(batch_size, -1)
 
+    # ==
+    # Gaussian analytical KLdiv
+    ele_kl = (logvar_p - logvar_q - 1.0
+              + (logvar_q.exp() / logvar_p.exp())
+              + ((mu_q - mu_p).pow(2) / logvar_p.exp()))  # (batch, input)
+    kld = (1.0/2.0) * torch.sum(ele_kl, dim=1)  # (batch, )
+
     # kld
-    return
+    return kld
 
 
 def kl_gaussian_gaussian_mc(mu_q, logvar_q, mu_p, logvar_p, num_samples=1):
@@ -121,5 +139,26 @@ def kl_gaussian_gaussian_mc(mu_q, logvar_q, mu_p, logvar_p, num_samples=1):
     mu_p = mu_p.view(batch_size, -1).unsqueeze(1).expand(batch_size, num_samples, input_size)
     logvar_p = logvar_p.view(batch_size, -1).unsqueeze(1).expand(batch_size, num_samples, input_size)
 
+    # ==
+    # Monte carlo kld
+
+    # Take sample from Q
+    stdev_q = (((1.0/2.0) * logvar_q).exp())
+    normal = torch.distributions.Normal(mu_q.float(), stdev_q.float())
+    z = normal.sample()
+
+    # Compute the relative density under Q and P
+    q_den = logvar_q + ((z-mu_q).pow(2) / logvar_q.exp())
+    p_den = logvar_p + ((z-mu_p).pow(2) / logvar_p.exp())
+    rel_den = q_den - p_den
+
+    # Compute KL
+    batch_sample_sum = torch.sum(rel_den, dim=2)  # (batch, num_samples)
+    batch_sum = torch.sum(batch_sample_sum, dim=1)  # (batch, )
+    kld = (-1.0/2.0) * (1.0/num_samples) * batch_sum
+
+    # TODO NOTE maybe fix: there may be catastrophic cancellation happening
+    # sometimes get negative values 
+
     # kld
-    return
+    return kld
